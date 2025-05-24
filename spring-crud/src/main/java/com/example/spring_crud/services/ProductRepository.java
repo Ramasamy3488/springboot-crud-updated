40b38e49ab868c4de1ad48938c3a398113227fb3
Ramasamy3488 <91617803+Ramasamy3488@users.noreply.github.com>
2025-05-24 20:54:15 +0530

    Delete spring-crud directory

--- spring-crud/src/main/java/com/example/spring_crud/services/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.spring_crud.services;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.spring_crud.models.Product;
-
-public interface ProductRepository extends JpaRepository<Product, Integer> {
-
-}
